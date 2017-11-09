5052e3389aafd231ea15bad8293b148e4f2a3f7f
p-hebert <philippe.hebert.qc@gmail.com>
2017-11-09 16:07:37 -0500

    Modified structure of integration test so that non-native tests are also run on the native platforms

--- test/src/macOS/index.js
+++ test/src/macOS/index.js
@@ -13,44 +13,50 @@ const ifElse = function ifElse (f, g, h) {
   return (x) => f(x) ? g(x) : h(x);
 };
 
-describe('macOS integration tests', ifElse(
-  eqeqeq('darwin'),
-  () => function(){
-    it('should properly parse the output of the filesystem command', function(done){
-      const input = child.execSync(macOS.COMMAND).toString();
-      const userFilter = () => true;
-      const acc = macOS.parser(userFilter)(input);
-      expect(acc.devices).to.be.an('object');
-      expect(Object.keys(acc.devices).length).to.be.at.least(1);
-      for(const k in acc.devices){
-        expect(acc.devices[k].id).to.be.a('string');
-        expect(acc.devices[k].node).to.be.a('string');
-        expect(acc.devices[k].size).to.be.a('number').that.is.gte(0);
-        expect(acc.devices[k].description).to.be.a('string');
-        expect(acc.devices[k].volumes).to.be.an('array');
-        acc.devices[k].volumes.forEach((v) => {
-          expect(typeof v.name === 'string' || v.name === null).to.be.true;
-          expect(v.description).to.be.null;
-          expect(v.mounted).to.be.a('boolean');
-          expect(v.space).to.be.an('object').that.has.all.keys('total', 'available', 'used');
-          expect(
-            (
-              v.space.total === null &&
-              v.space.available === null &&
-              v.space.used === null
-            ) ||
-            (
-              typeof v.space.total === 'number' && v.space.total >= 0 &&
-              typeof v.space.available === 'number' && v.space.available >= 0 &&
-              typeof v.space.used === 'number' && v.space.used >= 0
-            )
-          ).to.be.true;
-        });
-      }
-      done();
-    });
-  },
-  () => function(){
+describe('macOS integration tests', function() {
+  describe('native tests', ifElse(
+    eqeqeq('darwin'),
+    () => function(){
+      it('should properly parse the output of the filesystem command', function(done){
+        const input = child.execSync(macOS.COMMAND).toString();
+        const userFilter = () => true;
+        const acc = macOS.parser(userFilter)(input);
+        expect(acc.devices).to.be.an('object');
+        expect(Object.keys(acc.devices).length).to.be.at.least(1);
+        for(const k in acc.devices){
+          expect(acc.devices[k].id).to.be.a('string');
+          expect(acc.devices[k].node).to.be.a('string');
+          expect(acc.devices[k].size).to.be.a('number').that.is.gte(0);
+          expect(acc.devices[k].description).to.be.a('string');
+          expect(acc.devices[k].volumes).to.be.an('array');
+          acc.devices[k].volumes.forEach((v) => {
+            expect(typeof v.name === 'string' || v.name === null).to.be.true;
+            expect(v.description).to.be.null;
+            expect(v.mounted).to.be.a('boolean');
+            expect(v.space).to.be.an('object').that.has.all.keys('total', 'available', 'used');
+            expect(
+              (
+                v.space.total === null &&
+                v.space.available === null &&
+                v.space.used === null
+              ) ||
+              (
+                typeof v.space.total === 'number' && v.space.total >= 0 &&
+                typeof v.space.available === 'number' && v.space.available >= 0 &&
+                typeof v.space.used === 'number' && v.space.used >= 0
+              )
+            ).to.be.true;
+          });
+        }
+        done();
+      });
+    },
+    () => function() {
+      it('should not run native macOS tests', (done) => done());
+    }
+  )(os.platform()));
+
+  describe('non-native tests', function(){
     it('should properly parse the provided static input', function(done){
       const input = fs.readFileSync(path.resolve(__dirname, 'input.txt')).toString().replace('\n', os.EOL);
       const userFilter = () => true;
@@ -84,5 +90,5 @@ describe('macOS integration tests', ifElse(
       }
       done();
     });
-  }
-)(os.platform()));
+  });
+});
