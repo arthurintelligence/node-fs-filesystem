b620614e5a485f4b4b8a597e5a0593766b081fee
Javier Castro <javier.alejandro.castro@gmail.com>
2018-05-11 00:56:37 -0300

    done #5: refactored parsing for using wmic csv output; test pass (windows)

--- src/windows/windows.js
+++ src/windows/windows.js
@@ -1,47 +1,60 @@
 import F from '../functional';
-import { emptyDevice, emptyVolume, splitEOL } from '../utilities';
+import { emptyDevice, emptyVolume } from '../utilities';
+import csvSync from 'csv-parse/lib/sync';
 const { compose, reduce, filter } = F.R;
 
-export const COMMAND = 'wmic logicaldisk get ' +
-  'Caption,Description,DeviceID,DriveType,FileSystem,FreeSpace,Name,Size,VolumeName';
+export const COMMAND =
+  'wmic logicaldisk get ' +
+  'Caption,Description,DeviceID,DriveType,FileSystem,FreeSpace,Name,Size,VolumeName ' +
+  '/format:csv';
 
-export const parseWindowsProps =
-(acc, [ caption, desc, id, drivetype, filesystem, space, name, size, volumename ]) => {
-  acc.devices[name] = acc.devices[name] ? acc.devices[name] : emptyDevice();
-  acc.devices[name].id = id;
-  acc.devices[name].whole = true;
-  acc.devices[name].parent = id;
-  acc.devices[name].node = caption;
-  acc.devices[name].name = name;
-  acc.devices[name].size = parseInt(size);
-  acc.devices[name].description = desc;
-  acc.devices[name].removable = (drivetype === '2');
+export const parseWindowsProps = (
+  acc,
+  {
+    Caption,
+    Description,
+    DeviceID,
+    DriveType,
+    FileSystem,
+    FreeSpace,
+    Name,
+    Size,
+    VolumeName
+  }
+) => {
+  acc.devices[Name] = acc.devices[Name] ? acc.devices[Name] : emptyDevice();
+  acc.devices[Name].id = DeviceID;
+  acc.devices[Name].whole = true;
+  acc.devices[Name].parent = DeviceID;
+  acc.devices[Name].node = Caption;
+  acc.devices[Name].name = Name;
+  acc.devices[Name].size = parseInt(Size) || 0;
+  acc.devices[Name].description = Description;
+  acc.devices[Name].removable = DriveType === '2';
 
   const volume = emptyVolume();
-  volume.id = id;
-  volume.node = id;
-  volume.name = volumename || null;
-  volume.parent = id;
+  volume.id = DeviceID;
+  volume.node = DeviceID;
+  volume.name = VolumeName || null;
+  volume.parent = DeviceID;
   volume.mounted = true;
-  volume.mountPoint = name;
-  volume.fs = filesystem;
-  volume.space.total = parseInt(size);
-  volume.space.available = parseInt(space);
-  volume.space.used = parseInt(size) - parseInt(space);
-  acc.devices[name].volumes = [volume];
+  volume.mountPoint = Name;
+  volume.fs = FileSystem;
+  volume.space.total = parseInt(Size) || 0;
+  volume.space.available = parseInt(FreeSpace) || 0;
+  volume.space.used = volume.space.total - volume.space.available;
+  acc.devices[Name].volumes = [volume];
 
   return acc;
 };
 
-export const parseWindows = (parseWindowsProps) => (userFilter) => compose(
-  ({ devices }) => ({
-    devices: filter(userFilter, devices)
-  }),
-  reduce(
-    (acc, v) => parseWindowsProps(acc, v.split(/\t|\s{2,}/)),
-    { devices: {} }
-  ),
-  (a) => a.splice(1),
-  filter((s) => s.trim()),
-  splitEOL
-);
+export const parseWindows = parseWindowsProps => userFilter =>
+  compose(
+    ({ devices }) => ({
+      devices: filter(userFilter, devices)
+    }),
+    reduce((acc, propsObj) => parseWindowsProps(acc, propsObj), {
+      devices: {}
+    }),
+    csv => csvSync(csv, { columns: true })
+  );
